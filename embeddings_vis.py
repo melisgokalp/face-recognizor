7d7ed2bafedf992f4ba68c93c70abcc4780e2189
Melis Gökalp <melisgokalp@gmail.com>
2020-06-24 16:35:24 +0300

    added person names to scatter plots

--- embeddings_vis.py
+++ embeddings_vis.py
@@ -10,27 +10,29 @@ embeddings = []
 labels = []
 ix = 0
 n = 10
-
-for face in glob.glob("data/embeddings/*.npy")[:n]:
+names = []
+for face in glob.glob("data/embeddings/live/*.npy")[:n]:
     samples = np.load(face)
     embeddings.append(samples)
     labels.append(np.full(len(samples), ix))
     ix += 1
+    name = ((face.split("/")[-1]).split(".")[0])
+    names = names + [name]*len(samples)
 embeddings = np.concatenate(embeddings)
 labels = np.concatenate(labels)
 
 pca = PCA(n_components=3)
 reduced = pca.fit_transform(embeddings)
 print(np.cumsum(pca.explained_variance_ratio_))
-
 plot = plotly.graph_objs.Scatter(
     x=reduced[:, 0],
     y=reduced[:, 1],
     mode="markers",
     marker=dict(
         color=labels,  # set color equal to a variable
-        colorscale='Rainbow',
-    )
+        colorscale='Rainbow'
+    ),
+    text = names
 )
 
 threeplot = plotly.graph_objs.Scatter3d(
@@ -41,7 +43,8 @@ threeplot = plotly.graph_objs.Scatter3d(
     marker=dict(
         color=labels,  # set color equal to a variable
         colorscale='Rainbow',
-    )
+    ),
+    text = names
 )
 
 plotly.offline.plot([plot], filename="embeddings.html")
