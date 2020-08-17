0e51f17e1f876c7c365077063af683b49405666e
Melis Gökalp <melisgokalp@gmail.com>
2020-08-17 12:24:12 +0300

    test names changed

--- repl.py
+++ repl.py
@@ -283,7 +283,7 @@ if __name__ == "__main__":
         for i in tqdm(range(start, len(files)), total=len(files)):
             file = files[i]
             video_capture = cv2.VideoCapture(file)
-            testname = file.split("/")[-2].replace("_", " ")
+            testname = file.split("/")[-2] #.replace("_", " ")
             print(mode + testname + " for file " + file)
             name_to_idx = {idx_to_name[idx]: idx for idx in idx_to_name}
             recognize(clf, num_classes, idx_to_name, True, args["retrain"],  args["hide"])
